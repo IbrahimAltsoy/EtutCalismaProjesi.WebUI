32c7d7a13f69ad950ab443ff95ac82ed9089a67f
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-26 23:49:57 +0300

    implement logger

--- EtutCalismaProjesi.WebAPI/Program.cs
+++ EtutCalismaProjesi.WebAPI/Program.cs
@@ -3,6 +3,10 @@ using EtutCalismaProjesi.Data.Absract;
 using EtutCalismaProjesi.Data.Concreate;
 using EtutCalismaProjesi.Service.Absract;
 using EtutCalismaProjesi.Service.Concreate;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.MSSqlServer;
+using System.Collections.ObjectModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Buradan balýyor
+
+Logger log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt")
+    .CreateLogger();
+builder.Host.UseSerilog(log);
+
+// burada bitiyor
 builder.Services.AddDbContext<DatabaseContext>();
 
 builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
@@ -33,3 +46,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
